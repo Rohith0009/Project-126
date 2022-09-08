95c8b5eaa359386cdcbf3a8557e3db4ed74efca5
Rohith M <rohithmerala@gmail.com>
2022-09-08 19:44:14 +0530

    wqe

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
-function setup(){
-    canvas = createCanvas(300,300);
-    canvas.center()
+song = "";
+
+function preload() {
+  song = loadSound("music.mp3");
+}
+
+function setup() {
+  canvas = createCanvas(600, 550);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+}
+
+function draw() {
+  image(video, 0, 0, 600, 550);
+}
+
+function play() {
+  song.play();
 }
