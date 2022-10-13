506b850ab29af96897a98ed1de43354432db141a
Rohith M <rohithmerala@gmail.com>
2022-10-13 18:29:25 +0530

    fixes

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-score_left_wrist = "";
-_status = "";
+Song1 = "";
+Song2 = "";
+song1_status = "";
+song2_status = "";
+ScoreRightWrist = 0;
+ScoreLeftWrist = 0;
+left_wrist_y = 0;
+left_wrist_x = 0;
+right_wrist_y = 0;
+right_wrist_x = 0;
 
 function preload() {
   Song1 = loadSound("song1.mp3");
@@ -15,21 +23,6 @@ function setup() {
   pose_net.on("pose", gotposes);
 }
 
-function draw() {
-  image(video, 0, 0, 600, 550);
-  _status = Song1.isPlaying();
-  if (score_left_wrist <= 0.2) {
-    circle(left_wrist_x, left_wrist_y, 20);
-    Song2.stop();
-    Song1.play();
-    document.getElementById("song_name").innerHTML = "Song 1";
-  }
-}
-
-function play() {
-  song.play();
-}
-
 function modelloaded() {
   console.log("Model Is Loaded");
 }
@@ -37,6 +30,8 @@ function modelloaded() {
 function gotposes(results) {
   if (results.length > 0) {
     ScoreLeftWrist = results[0].pose.keypoints[9].score;
+    ScoreRightWrist = results[0].pose.keypoints[10].score;
+    console.log("ScoreLeftWrist: " + ScoreLeftWrist + "ScoreRightWrist: " + ScoreRightWrist);
     console.log(results);
     left_wrist_x = results[0].pose.leftWrist.x;
     left_wrist_y = results[0].pose.leftWrist.y;
@@ -45,4 +40,34 @@ function gotposes(results) {
     right_wrist_y = results[0].pose.rightWrist.y;
     console.log("Right Wrist X is " + right_wrist_x + " Right Wrist Y is " + right_wrist_y);
   }
+}
+
+function draw() {
+  image(video, 0, 0, 600, 550);
+  song1_status = Song1.isPlaying();
+  song2_status = Song2.isPlaying();
+  fill("#00000");
+  stroke("#fffff");
+  if (ScoreLeftWrist > 0.2) {
+    circle(left_wrist_x, left_wrist_y, 20);
+    Song2.stop();
+    if (song1_status == false) {
+      Song1.play();
+      document.getElementById("song_name").innerHTML = "Song 1";
+    }
+  }
+  if (ScoreRightWrist > 0.2) {
+    circle(right_wrist_x, right_wrist_y, 20);
+    Song1.stop();
+    if (song2_status == false) {
+      Song2.play();
+      document.getElementById("song_name").innerHTML = "Song 2";
+    }
+  }
+}
+
+function play() {
+  song.play();
+  song.setVolume(1);
+  song.rate(1);
 }
